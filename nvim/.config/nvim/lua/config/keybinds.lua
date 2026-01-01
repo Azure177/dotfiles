aa42e1bade321267d6522eb14afad114c48060bc
Azure177 <azure4life078@gmail.com>
2026-01-02 00:30:12 +0530

    Now

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -6,3 +6,4 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<C-c>", ":nohl<CR>") --clears search highlights
 vim.keymap.set("n", "x", '"_x')
 
+vim.keymap.set("n", "<leader>t", ":terminal")
